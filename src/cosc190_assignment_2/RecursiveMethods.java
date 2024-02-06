0ac7b9a5d1764e29005f78647bcb63933d9fb6c0
theoinsaskpolytech <tran0885@saskpolytech.ca>
2024-02-06 16:30:22 -0600

    Done Q2

--- src/cosc190_assignment_2/RecursiveMethods.java
+++ src/cosc190_assignment_2/RecursiveMethods.java
@@ -4,12 +4,25 @@ public class RecursiveMethods {
 
     public static String reverse(String sText) {
         if (sText.length() <= 1) return sText;
-        String reversed = reverse(sText.substring(1)) + sText.charAt(0);
-        return reversed;
+        return reverse(sText.substring(1)) + sText.charAt(0);
     }
 
     public static int bin2Dec(String binNumber) {
-        return 0;
+        if (binNumber.length() <= 1) return Integer.parseInt(binNumber);
+        return (int) (bin2Dec(binNumber.substring(1)) + Integer.parseInt("" + binNumber.charAt(0)) * Math.pow(2, binNumber.length() - 1));
+    }
+
+    public static String justBinary(String binNumber) {
+        StringBuilder onlyBin = new StringBuilder();
+        for (int i = 0; i < binNumber.length(); i++) {
+            if (Character.isDigit(binNumber.charAt(i))) {
+                int num = Integer.parseInt("" + binNumber.charAt(i));
+                if (num == 0 || num == 1) {
+                    onlyBin.append(binNumber.charAt(i));
+                }
+            }
+        }
+        return onlyBin.toString();
     }
 
     public static void replaceAll(String oldWord, String newWord, String filePath) {
