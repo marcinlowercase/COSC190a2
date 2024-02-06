987cddb23a5ccc7a666e75d4a49ce904ce33a926
theoinsaskpolytech <tran0885@saskpolytech.ca>
2024-02-06 15:59:55 -0600

    Q1 Super short

--- src/cosc190_assignment_2/RecursiveMethods.java
+++ src/cosc190_assignment_2/RecursiveMethods.java
@@ -4,10 +4,8 @@ public class RecursiveMethods {
 
     public static String reverse(String sText) {
         if (sText.length() <= 1) return sText;
-        StringBuilder reversed = new StringBuilder();
-        reversed.append(reverse(sText.substring(1)));
-        reversed.append(sText.charAt(0));
-        return reversed.toString();
+        String reversed = reverse(sText.substring(1)) + sText.charAt(0);
+        return reversed;
     }
 
     public static int bin2Dec(String binNumber) {
