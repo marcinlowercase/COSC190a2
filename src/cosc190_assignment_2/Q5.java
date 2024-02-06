9d115438760b440e125f64890398f56e8688b784
theoinsaskpolytech <tran0885@saskpolytech.ca>
2024-02-06 17:14:45 -0600

    Done Q5

--- src/cosc190_assignment_2/Q5.java
+++ src/cosc190_assignment_2/Q5.java
@@ -1,5 +1,6 @@
 package cosc190_assignment_2;
 
+import java.util.Arrays;
 import java.util.function.Function;
 
 public class Q5 {
@@ -11,9 +12,9 @@ public class Q5 {
     public static void main(String[] args) {
         Integer[] aVals = {2, 7, 10, 1};
 
-//        System.out.printf("The first value is %d\n", doOperation(getFirst, aVals));
-//        System.out.printf("The Last value is %d\n", doOperation(getLast, aVals));
-//        System.out.printf("The Average value is %d\n", doOperation(getAverage, aVals));
+        System.out.printf("The first value is %d\n", doOperation(arr -> arr[0], aVals));
+        System.out.printf("The Last value is %d\n", doOperation(arr -> arr[arr.length -1], aVals));
+        System.out.printf("The Average value is %d\n", doOperation(arr -> (int) Arrays.stream(arr).mapToInt(Integer::intValue).average().getAsDouble(), aVals));
 
     }
 
