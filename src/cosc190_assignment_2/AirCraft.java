5c5b9aaaaa72b990d1fd0a694166d2c16f0d1663
theoinsaskpolytech <tran0885@saskpolytech.ca>
2024-02-07 14:55:08 -0600

    Q4 All work but not understand yet

--- src/cosc190_assignment_2/AirCraft.java
+++ src/cosc190_assignment_2/AirCraft.java
@@ -1,6 +1,91 @@
 package cosc190_assignment_2;
 
+import java.util.*;
+import java.util.stream.Stream;
+
 public class AirCraft {
+    private String type;
+    private List<String> subTypes;
+    private String name;
+    private int inService;
+    private String country;
 
+//    public AirCraft(String[] sArgs) {
+//        if ( sArgs.length == 5){
+//            this.type = sArgs[TYPE];
+//            this.subTypes.add(sArgs[SUBTYPES]);
+//            this.name = sArgs[NAME];
+//            this.inService = Integer.parseInt(sArgs[INSERVICE]);
+//            this.country = sArgs[COUNTRY];
+//        } else {
+//            this.type = sArgs[0];
+//            String subRaw = Arrays.toString(Arrays.copyOfRange(sArgs,1, sArgs.length - 3));
+//            this.subTypes = Arrays.stream(subRaw.split(",|and"))
+//                    .map(x -> x.replaceAll("[\"\\[\\]]", "" ))
+//                    .map(String::trim)
+//                    .map(s -> s.substring(0,1).toUpperCase() + s.substring(1))
+//                    .toList();
+//            this.name = sArgs[sArgs.length - 3];
+//            this.inService = Integer.parseInt(sArgs[sArgs.length - 2]);
+//            this.country = sArgs[sArgs.length - 1];
+//        }
+//    }
 
+    public AirCraft(String[] sArgs) {
+            this.type = sArgs[0];
+            String subRaw = Arrays.toString(Arrays.copyOfRange(sArgs,1, sArgs.length - 3));
+            this.subTypes = Arrays.stream(subRaw.split(","))
+                    .map(x -> x.replaceAll("[\"\\[\\]]", "" ))
+                    .map(String::trim)
+                    .map(s -> s.substring(0,1).toUpperCase() + s.substring(1))
+                    .toList();
+            this.name = sArgs[sArgs.length - 3];
+            this.inService = Integer.parseInt(sArgs[sArgs.length - 2]);
+            this.country = sArgs[sArgs.length - 1];
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getInService() {
+        return inService;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public List<String> getSubTypes() {
+        return subTypes;
+    }
+
+    public boolean isOfSubtype(String sSubType){
+        return this.subTypes.stream()
+                .anyMatch(str -> str.compareToIgnoreCase(sSubType) == 0);
+    }
+
+    @Override
+    public boolean equals(Object obOther) {
+        if (this == obOther) return true;
+        if (obOther == null || getClass() != obOther.getClass()) return false;
+        AirCraft airCraft = (AirCraft) obOther;
+        return inService == airCraft.inService && Objects.equals(type, airCraft.type) && Objects.equals(subTypes, airCraft.subTypes) && Objects.equals(name, airCraft.name) && Objects.equals(country, airCraft.country);
+    }
+
+    @Override
+    public String toString() {
+        return "AirCraft{" +
+                "type='" + type + '\'' +
+                ", subTypes=" + subTypes +
+                ", name='" + name + '\'' +
+                ", inService=" + inService +
+                ", country='" + country + '\'' +
+                '}';
+    }
 }
+
--- src/cosc190_assignment_2/AirQuery.java
+++ src/cosc190_assignment_2/AirQuery.java
@@ -1,48 +1,96 @@
 package cosc190_assignment_2;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class AirQuery {
 
-    public static List<AirCraft> loadInfo(String path) {
-
-        return null;
+    public static List<AirCraft> loadInfo(String path) throws IOException {
+        try (Stream<String> lines = Files.lines(Paths.get(path))){
+            return lines.skip(1)
+                    .map(line -> line.split(","))
+                    .map(arr -> new AirCraft(arr))
+                    .collect(Collectors.toList());
+        }
     }
 
     public static List<String> getCountryList(List<AirCraft> airCraftList) {
-        return null;
+        return airCraftList.stream()
+                .map(airCraft -> airCraft.getCountry())
+                .distinct()
+                .sorted()
+                .collect(Collectors.toList());
     }
 
     public static ArrayList<String> identifyTypes(List<AirCraft> airCraftList) {
-        return null;
+        return (ArrayList<String>) airCraftList.stream()
+                .map(airCraft -> airCraft.getType())
+                .distinct()
+                .collect(Collectors.toList());
     }
 
     public static int getCount(List<AirCraft> airCraftList, String country) {
-        return 0;
+        return (int) airCraftList.stream()
+                .filter(ct -> ct.getCountry().equals(country))
+                .count();
     }
 
     public static List<AirCraft> getListByType(List<AirCraft> airCraftList, String type) {
-        return null;
+        return airCraftList.stream()
+                .filter(t -> t.getType().equals(type))
+                .collect(Collectors.toList());
     }
 
     public static List<AirCraft> getListByTypeSortYear(List<AirCraft> airCraftList, String type) {
-        return null;
+        return airCraftList.stream()
+                .filter(t -> t.getType().equals(type))
+                .sorted(Comparator.comparingInt(AirCraft::getInService))
+                .collect(Collectors.toList());
     }
 
     public static List<String> getNamesBySubTypeYear(List<AirCraft> airCraftList, String subType, int year) {
-        return null;
+        return airCraftList.stream()
+                .filter(a -> a.isOfSubtype(subType))
+                .filter(y -> y.getInService()== year)
+                .map(n -> n.getName())
+                .collect(Collectors.toList());
+
     }
 
     public static List<String> getCountByCountry(List<AirCraft> airCraftList) {
-        return null;
+        return airCraftList.stream()
+                .collect(Collectors.groupingBy(AirCraft::getCountry, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .map(a -> a.getKey() + ":" + a.getValue())
+                .collect(Collectors.toList());
     }
 
     public static List<String> getCountByType(List<AirCraft> airCraftList, int year) {
-        return null;
+        return airCraftList.stream()
+                .filter(y -> y.getInService() == year)
+                .collect(Collectors.groupingBy(AirCraft::getType, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .map(a -> a.getKey() + ":" + year + ":" + a.getValue())
+                .collect(Collectors.toList());
     }
 
     public static List<String> getSubTypes(List<AirCraft> airCraftList){
-        return null;
+        return airCraftList.stream()
+                .map(AirCraft::getSubTypes)
+                .reduce(new ArrayList<>(), (a, b) -> {
+                    a.addAll(b);
+                    return a;
+                })
+                .stream()
+                .distinct()
+                .collect(Collectors.toList());
     }
 }
